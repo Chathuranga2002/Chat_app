e7fb4252af0e5b0f8eeba2d1ad31fc7270177c71
Chathuranga2002 <121780050+Chathuranga2002@users.noreply.github.com>
2023-05-30 05:26:17 +0530

    client view update

---
+++ Client/src/controller/Client2FormController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class Client2FormController {
+    @FXML
+    private Label nameId;
+
+    @FXML
+    private TextField txtFeld;
+
+    @FXML
+    private ScrollPane msgContent;
+
+    @FXML
+    void ExitOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void SendBtnOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imgOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imojiEvent(MouseEvent event) {
+
+    }
+
+    @FXML
+    void sendOnAction(ActionEvent event) {
+
+    }
+}
