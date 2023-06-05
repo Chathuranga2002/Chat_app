e1d10f605eaac4b2b404c43ccd83519e038e2096
Chathuranga2002 <121780050+Chathuranga2002@users.noreply.github.com>
2023-06-05 21:27:18 +0530

    client view update

--- Server/src/ServerAppIniselizer.java
+++ Server/src/ServerAppIniselizer.java
@@ -1,5 +1,11 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class ServerAppIniselizer extends Application {
 
@@ -8,7 +14,13 @@ public class ServerAppIniselizer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        URL resource = getClass().getResource("view/ServerForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Stage stage = new Stage();
+        Scene scene = new Scene(load);
+        stage.setScene(scene);
+        stage.show();
 
     }
 }
